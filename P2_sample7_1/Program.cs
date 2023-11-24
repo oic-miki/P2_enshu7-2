0250b8ef61c45b6bb5f351732b0eaee9cf5c3376
miki <miki@oic.jp>
2023-11-24 15:45:46 +0900

    SQLで注文状況をひとまとめにしてロジックで合計を計算

--- P2_sample7_1/Program.cs
+++ P2_sample7_1/Program.cs
@@ -1,72 +1,74 @@
 ﻿using Microsoft.Data.SqlClient;
-
-// パラメータ用文字列の入力
-//Console.Write("Idを入力してください＞");
-//int input = int.Parse(Console.ReadLine());
+using System.Diagnostics;
 
 // 接続文字列の設定
 
+// "Sales.mdf"
 string connectionString = @"";
 
 // データベースへの接続準備
 using (SqlConnection conn = new SqlConnection())
 {
+
     conn.ConnectionString = connectionString;
 
     // コマンドの準備
-    string query = "SELECT Id, name, value FROM m_shouhin;";
-    //string query = "SELECT Id, name, value FROM m_shouhin WHERE Id=@id;";
+    string query = "SELECT Orders.productId, Products.name, Products.price, Orders.quantity FROM Products, (SELECT Orders.productId, SUM(Orders.quantity) AS quantity FROM Orders, Products WHERE Orders.productId = Products.Id GROUP BY Orders.productId) Orders WHERE Products.Id = Orders.productId ORDER BY Products.Id;";
+
     using (SqlCommand cmd = new SqlCommand())
     {
+
         cmd.Connection = conn;
         cmd.CommandText = query;
 
         // データベース処理は失敗する可能性があるので、例外処理の準備
         try
         {
+
             // データベースへの接続
             conn.Open();
-
-            // パラメータの設定
-            //cmd.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = input;
 
             // コマンドの実行
             using (SqlDataReader reader = cmd.ExecuteReader())
             {
+
                 // 行を読む
                 while (reader.Read())
                 {
+
                     // 値を取り出す
-                    int id = reader.GetInt32(0);
+                    int productId = reader.GetInt32(0);
                     string name = reader.GetString(1);
-                    int value = reader.GetInt32(2);
+                    int price = reader.GetInt32(2);
+                    int quantity = reader.GetInt32(3);
 
                     // 取り出した値を画面に書く
-                    Console.WriteLine($"Id:{id}, name:{name}, value:{value}");
+                    Console.WriteLine($"productId:{productId}, name:{name}, price:{price}, quantity:{quantity}");
+                    Console.WriteLine($"売上合計:{price * quantity}");
+
                 }
-                // パラメータを使った場合の処理
-                //if (reader.HasRows)
-                //{
-                //    reader.Read();
-                //    string name = reader.GetString(1);
-                //    Console.WriteLine($"商品は{name}ですね！");
-                //}
-                //else
-                //{
-                //    Console.WriteLine("商品が見つかりませんでした。");
-                //}
 
                 // 実行結果の後処理
-                //reader.Close();
+                reader.Close();
+
             }
+
         }
-        catch
+        catch (Exception ex)
         {
+
             // 例外が出た場合の処理
-            Console.WriteLine("エラーが発生しました。");
+            Console.WriteLine("エラーが発生しました。[{0}]", ex.Message);
+
+        }
+        finally
+        {
+
+            // データベースへの接続解除
+            conn.Close();
+
         }
 
-        // データベースへの接続解除
-        //conn.Close();
     }
+
 }
